f4b95638d0afe8121fc8574f5668ca5266c9d883
FATIMA EZZAHRAE BEN HSSAIN <fatimaezzahrae.benhssain@usmba.ac.ma>
2022-12-02 20:15:04 +0100

    Calculator application

--- app/src/main/java/ma/ensaf/mycalculator/MainActivity.java
+++ app/src/main/java/ma/ensaf/mycalculator/MainActivity.java
@@ -1,100 +1,179 @@
 package ma.ensaf.mycalculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
-
+import org.mariuszgromada.math.mxparser.*;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    TextView sol,result;
-    MaterialButton bt1,bt2,bt3,bt5,bt6,bt7,bt8,bt9,bt10,bt11,bt12,bt13,bt14
-            ,bt15,bt16,bt17,bt18,bt19,bt20,bt21,bt22,bt23,bt24;
-    ImageButton bt4;
+public class MainActivity extends AppCompatActivity  {
     @SuppressLint("MissingInflatedId")
+
+    private TextView sol;
+    private EditText result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bt1= (MaterialButton) findViewById(R.id.bt1);
-        bt2= (MaterialButton) findViewById(R.id.bt2);
-        bt4= (ImageButton) findViewById(R.id.bt4);
-        bt5= (MaterialButton) findViewById(R.id.bt5);
-        bt6= (MaterialButton) findViewById(R.id.bt6);
-        bt7= (MaterialButton) findViewById(R.id.bt7);
-        bt8= (MaterialButton) findViewById(R.id.bt8);
-        bt9= (MaterialButton) findViewById(R.id.bt9);
-        bt10= (MaterialButton) findViewById(R.id.bt10);
-        bt11= (MaterialButton) findViewById(R.id.bt11);
-        bt12= (MaterialButton) findViewById(R.id.bt12);
-        bt13= (MaterialButton) findViewById(R.id.bt13);
-        bt14= (MaterialButton) findViewById(R.id.bt14);
-        bt15= (MaterialButton) findViewById(R.id.bt15);
-        bt16= (MaterialButton) findViewById(R.id.bt16);
-        bt17= (MaterialButton) findViewById(R.id.bt17);
-        bt18= (MaterialButton) findViewById(R.id.bt18);
-        bt19= (MaterialButton) findViewById(R.id.bt19);
-        bt20= (MaterialButton) findViewById(R.id.bt20);
-        bt21= (MaterialButton) findViewById(R.id.bt21);
-        bt22= (MaterialButton) findViewById(R.id.bt22);
-        bt23= (MaterialButton) findViewById(R.id.bt23);
-        bt24= (MaterialButton) findViewById(R.id.bt24);
 
-        result=(TextView)findViewById(R.id.result_tv);
-        sol=(TextView)findViewById(R.id.solution_tv);
+        sol = findViewById(R.id.solution_tv);
+        result = findViewById(R.id.result_tv);
 
-        bt3= (MaterialButton) findViewById(R.id.bt3);
+        result.setShowSoftInputOnFocus(false);
+    }
+
+    private void updateText(String strToAdd){
+        String oldStr = result.getText().toString();
+        int cursorPos = result.getSelectionStart();
+        String leftStr = oldStr.substring(0, cursorPos);
+        String rightStr = oldStr.substring(cursorPos);
+
+        result.setText(String.format("%s%s%s", leftStr, strToAdd, rightStr));
+        result.setSelection(cursorPos + strToAdd.length());
+    }
+
+    public void zero(View view){
+        updateText(getResources().getString(R.string.zero));
+    }
+
+    public void one(View view){
+        updateText(getResources().getString(R.string.one));
+    }
+
+    public void two(View view){
+        updateText(getResources().getString(R.string.two));
+    }
+
+    public void three(View view){
+        updateText(getResources().getString(R.string.three));
+    }
+
+    public void four(View view){
+        updateText(getResources().getString(R.string.four));
+    }
+
+    public void five(View view){
+        updateText(getResources().getString(R.string.five));
+    }
+
+    public void six(View view){
+        updateText(getResources().getString(R.string.six));
+    }
+
+    public void seven(View view){
+        updateText(getResources().getString(R.string.seven));
+    }
+
+    public void eight(View view){
+        updateText(getResources().getString(R.string.eight));
+    }
+
+    public void nine(View view){
+        updateText(getResources().getString(R.string.nine));
+    }
+
+    public void multiply(View view){
+        updateText(getResources().getString(R.string.multiply));
+    }
+
+    public void divide(View view){
+        updateText(getResources().getString(R.string.divide));
+    }
+
+    public void subtract(View view){
+        updateText(getResources().getString(R.string.subtract));
+    }
+
+    public void add(View view){
+        updateText(getResources().getString(R.string.add));
+    }
+
+    public void clearall(View view){
+       result.setText("");
+       sol.setText("");
+    }
+    
+    public void decimal(View view){
+        updateText(getResources().getString(R.string.decimal));
+    }
+
+    public void equal(View view){
+        String userExp = result.getText().toString();
+
+        sol.setText(userExp);
+
+        userExp = userExp.replaceAll(getResources().getString(R.string.divide), "/");
+        userExp = userExp.replaceAll(getResources().getString(R.string.multiply), "*");
+
+        Expression exp = new Expression(userExp);
+        String resultat = String.valueOf(exp.calculate());
+
+        result.setText(resultat);
+        result.setSelection(result.length());
+    }
+
+    public void backspace(View view){
+        int cursorPos = result.getSelectionStart();
+        int textLen = result.getText().length();
+
+        if (cursorPos != 0 && textLen != 0){
+            SpannableStringBuilder selection = (SpannableStringBuilder) result.getText();
+            selection.replace(cursorPos-1, cursorPos, "");
+            result.setText(selection);
+            result.setSelection(cursorPos-1);
+        }
+    }
+
+    public void sqrt(View view){
+        updateText("sqrt(");
+    }
+    public void xSquared(View view){
+        updateText("^(2)");
+    }
+    public void oposite(View view){
+        updateText(getResources().getString(R.string.multiply)+"(-1)");
+    }
+    public void inverte(View view){
+        updateText("^(-1)");
+    }
+    public void open(View view){
+        updateText(getResources().getString(R.string.parenthesesOpen));
+    }
+    public void close(View view){
+        updateText(getResources().getString(R.string.parenthesesClose));
     }
 
     @Override
-    public void onClick(View view) {
-        MaterialButton button =(MaterialButton) view;
-        String buttonText=button.getText().toString();
-        //sol.setText(buttonText);
-        result.setText(buttonText);
-        String calculate=sol.getText().toString();
-
-        if(buttonText.equals("AC")){
-            sol.setText("");
-            result.setText("0");
-            return;
-        }
-        else if(buttonText.equals("=")){
-            sol.setText(result.getText());
-            return;
-        }
-        else if(buttonText.equals("C")){
-            calculate=calculate.substring(0,calculate.length()-1);
-        }
-        else {
-            calculate += buttonText;
-        }
-        sol.setText(calculate);
-        String finalResult=getResultat(calculate);
-        if(!finalResult.equals("error")){
-            result.setText(finalResult);
-        }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
     }
-    String getResultat(String data){
-        try{
-            Context context=Context.enter();
-            context.setOptimizationLevel(-1);
-            Scriptable scriptable =context.initStandardObjects();
-            String endresult=context.evaluateString(scriptable,data
-                    ,"Javascript",1,null).toString();
-            if(endresult.endsWith(".0"))
-                endresult=endresult.replace(".0","");
-            return endresult;
-        }
-        catch(Exception e) {
-            return "error";
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_calculator1:
+                Toast.makeText(this, "scientific selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_calculator2:
+                Toast.makeText(this, "settings selected", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
